f9c6cc16c3fae7c540724c4f03b4e5172ef32fab
Виталий <dom200989@gmail.com>
2021-10-22 21:11:55 +0300

    переименование

--- src/main/java/com/example/zookeeping/ZooKeepingApplication.java
+++ src/main/java/com/example/zookeeping/ZooKeepingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZookeepingApplication {
+public class ZooKeepingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZookeepingApplication.class, args);
+		SpringApplication.run(ZooKeepingApplication.class, args);
 	}
 
 }
